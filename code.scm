08c8f1dec8e719e320f0895a17a3dbe135a6a206
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-17 14:25:34 +0530

    ex1.12

--- code.scm
+++ code.scm
@@ -63,6 +63,13 @@
   (iter 0 1 2 n))
 
 
+;; ex 1.12
+(define (pascal row col)
+  (cond
+    ((or (= col row) (= col 0)) 1)
+    (else (+ (pascal (- row 1) (- col 1))
+             (pascal (- row 1) col)))))
 
+                    
 
   
