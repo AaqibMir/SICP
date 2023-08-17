1e01476150c20847ba22d199aa01210dd3803cc5
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-17 12:27:18 +0530

    ex1.7

--- code.scm
+++ code.scm
@@ -11,3 +11,28 @@
 
 (define (square x) (* x x))
 
+;; ex 1.7
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y))) 1))
+
+(define (average-damp f)
+  (lambda (a)
+    (/ (+ a (f a)) 2)))
+
+(define (fixed-point f guess)
+  
+  (define (iter old new)
+    (if (good-enough? old new)
+        new
+        (iter new (f new))))
+  
+  (define (good-enough? x y)
+    (< (/ (abs (- x y)) y) tolerance))
+  
+  (define tolerance 0.0001)
+  
+  (iter guess (f guess)))
+
+
+
+  
