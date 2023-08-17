9e9e6fd515de4d300a2a4e0e92592a27623a7567
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-17 12:01:29 +0530

    ex1.3

--- code.scm
+++ code.scm
@@ -1,1 +1,13 @@
 ;; Learning to code
+
+#lang sicp
+
+;; ex1.3
+(define (f a b c)
+  (cond
+    ((< a b)(f b a c))
+    ((< b c)(f a c b))
+    (else (+ (square a) (square b)))))
+
+(define (square x) (* x x))
+
