b70cbb076072b03d62aa84d4cbe73fa2fa34c530
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-19 18:14:59 +0530

    ex1.27 Carmichael numbers fool fermat's test

--- code.scm
+++ code.scm
@@ -110,6 +110,7 @@
   (display n)
   (start-prime-test n (runtime)))
 (define (start-prime-test n start-time)
+  (define (prime? n) (fast-prime? n 3))
 
   (if (prime? n)
       (report-prime (- (runtime) start-time)))
@@ -132,6 +133,45 @@
   (if (= test-divisor 2)
       (+ test-divisor 1)
       (+ test-divisor 2)))
+
+;; ex 1.24
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+;; ex 1.2
+;; Numbers that fool the Fermat test are called Carmichael numbers. Fermat's test
+;; implies they are prime, but they are not. e.g. 561, 1105, 6601
+(define (congruent? a n)
+  (= (expmod a n n) a))
+(define (is-it-prime-fermat? n)
+  (define (iter a n)
+    (cond
+      ((= a n) #t)
+      ((congruent? a n) (iter (+ a 1) n))
+      (else #f)))
+        
+  (iter 2 n))
+      
+
+  
+
+
+
       
 
 
