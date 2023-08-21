104717fd472a7ecdf5822d6e1c3ab2afff7e48c6
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-21 09:19:38 +0530

    ex1.32

--- code.scm
+++ code.scm
@@ -254,5 +254,27 @@
            n)))
 
                     
+;; ex 1.32
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term
+                            (next a) next b))))
+
+(define (sum-accumulate term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product-accumulate term a next b)
+  (accumulate * 1 term a next b))
+
+;; iterative version
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
 
   
