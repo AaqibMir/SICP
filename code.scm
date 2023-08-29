c81ff762d93845f4b4390e1085b92c06601a2a47
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-29 10:44:51 +0530

    ex2.19

--- code.scm
+++ code.scm
@@ -707,3 +707,29 @@
         rev
         (helper (cdr lst) (cons (car lst) rev))))
   (helper lst nil))
+
+;; ex 2.19
+(define us-coins (reverse (list 50 25 10 5 1)))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+
