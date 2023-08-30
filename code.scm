2ea59395a318bb072c631972f4a1aa118dfd0756
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-30 12:11:39 +0530

    ex2.27

--- code.scm
+++ code.scm
@@ -765,3 +765,13 @@
     (else (proc (car things))
           (for-each- proc (cdr things)))))
       
+;; ex 2.27
+(define (deep-reverse lst)
+  (cond
+    ((null? lst) '())
+    ((pair? (car lst)) (append (deep-reverse (cdr lst))
+                               (list (deep-reverse (car lst)))))
+    (else (append (deep-reverse (cdr lst))
+                  (list (car lst))))))
+
+
