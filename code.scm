f55e921e262cf2419da9122edef0d188a2d31dd8
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-31 10:35:08 +0530

    ex2.33

--- code.scm
+++ code.scm
@@ -882,3 +882,19 @@
         (append rest (map (lambda (x)
                             (cons (car s) x))
                           rest)))))
+
+
+;; ex 2.33
+(define (accumulate- op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate- op initial (cdr sequence)))))
+(define (map- p seq)
+  (accumulate- (lambda (x y) (cons (p x) y)) nil seq))
+
+(define (append- seq1 seq2)
+  (accumulate- cons seq2 seq1))
+
+(define (length seq)
+  (accumulate- (lambda (x y) (+ 1 y)) 0 seq))
