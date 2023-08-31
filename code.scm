2c15e4c36874fddb8c9f6f253678d8cf5aeab04b
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-08-31 11:55:33 +0530

    ex2.36

--- code.scm
+++ code.scm
@@ -919,6 +919,13 @@
                    t)))
 
 
-
+;; ex 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate- op init (map (lambda (x)
+                                        (car x)) seqs))
+            (accumulate-n op init (map (lambda (x)
+                                         (cdr x)) seqs)))))
 
          
