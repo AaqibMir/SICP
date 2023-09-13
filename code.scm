5e9090468adde292bdbf5e2bf4817d434576b8a0
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-09-13 12:15:11 +0530

    ex2.46

--- code.scm
+++ code.scm
@@ -1104,5 +1104,30 @@
 (define right-split-new (split beside below))
 (define up-split-new (split below beside))
 
-            
-  
+;; ex 2.46
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cadr v))
+
+;; add vectors
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1)
+           (xcor-vect v2))
+        (+ (ycor-vect v1)
+           (ycor-vect v2))))
+;; subtract vectors
+ (define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1)
+           (xcor-vect v2))
+        (- (ycor-vect v1)
+           (ycor-vect v2))))
+
+;; scale vectors
+(define (scale-vect v s)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
