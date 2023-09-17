a18fa831491e8731799e21a64e4d43e9ac6933ab
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-09-17 20:12:44 +0530

    ex2.51

--- code.scm
+++ code.scm
@@ -1086,17 +1086,19 @@
         (paint-left frame)
         (paint-right frame)))))
 
+
+;; ex 2.51a
 (define (below painter1 painter2)
   (let ((split-point (make-vect 0 0.5)))
     (let ((paint-up
            (transform-painter painter2
                               split-point
-                              (make-vect 0.5 1.0)
-                              (make-vect 0.5 1.0)))
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0)))
           (paint-down
            (transform-painter painter1
                               (make-vect 0.0 0.0)
-                              (make-vect 0.0 1.0)
+                              (make-vect 1.0 0.0)
                               split-point)))
       (lambda (frame)
         (paint-up frame)
@@ -1370,4 +1372,12 @@
 		     (make-vect 0.0 1.0)
 		     (make-vect 0.0 0.0)
 		     (make-vect 1.0 1.0)))
+
+
+;; ex 2.51b
+(define (beside-b painter1 painter2 rotation-1 rotation-2)
+  (beside (rotation-1 painter1) (rotation-2 painter2)))
+
+
+  
 
