9a7159d893ec4886ccf2c9dee62e62276e44023a
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-09-17 19:37:42 +0530

    ex2.50

--- code.scm
+++ code.scm
@@ -1057,9 +1057,50 @@
 
 ;; code from the book
 ;; temporary definitions of beside and below
-(define (below x y) (lambda (frame) (x frame)(y frame)))	    
+;; (define (below x y) (lambda (frame) (x frame)(y frame)))	    
 			    
-(define (beside x y) (lambda (frame) (x frame)(y frame)))
+;; (define (beside x y) (lambda (frame) (x frame)(y frame)))
+
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0 0.5)))
+    (let ((paint-up
+           (transform-painter painter2
+                              split-point
+                              (make-vect 0.5 1.0)
+                              (make-vect 0.5 1.0)))
+          (paint-down
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 0.0 1.0)
+                              split-point)))
+      (lambda (frame)
+        (paint-up frame)
+        (paint-down frame)))))
 
 (define (right-split painter n)
   (if (= n 0)
@@ -1279,7 +1320,7 @@
   (outline-diamond frame))
 
 
-
+;; took the segment list at http://community.schemewiki.org/?sicp-ex-2.49
 (define segment-list-wave
   (list
    (make-segment (make-vect .25 0) (make-vect .35 .5))
@@ -1310,4 +1351,23 @@
 ;; in it.
 (define outline-wave (segments->painter segment-list-wave))
 
+;; ex 2.50
+
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
 
