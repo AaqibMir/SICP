222d809362cbd57bdcffa943ad367a62cf0b88da
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-09-18 20:16:37 +0530

    ex2.56

--- code.scm
+++ code.scm
@@ -1270,7 +1270,10 @@
 ;; on your screen. This is where the painter will draw its painting.
 ;; This window is named 'window' below and will be used by plot-line in
 ;; draw-line procedure.
-(define window (frame -5 15 -5 15))
+
+;; enable this definition if you want to see the paintings
+;; painted by the painters
+;; (define window (frame -5 15 -5 15))
 
 
 (define (draw-line a b)
@@ -1461,6 +1464,94 @@
    (else (and (equal? (car l1) (car l2))
 	      (equal? (cdr l1) (cdr l2))))))))
 
+;; code from book
+(define (variable? x) (symbol? x))
 
-    
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+       (variable? v2)
+       (eq? v1 v2)))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s)
+  (if (= (length s) 3) (caddr) (cons '+ (caddr s))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p)
+  (cadr p))
+(define (multiplicand p)
+  (caddr p))
+
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+
+
+
+
+
+;; ex 2.56
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+	((exponentiation? exp)
+	 (cond
+	  ((= 0 (exponent exp)) (deriv 1 var))
+	  ((= 1 (exponent exp)) (deriv (base exp) var))
+	  (else
+	   (make-product (exponent exp)
+			 (make-product (make-exponentiation (base exp)
+							    (- (exponent exp) 1))
+				       (deriv (base exp) var))))))
+	 
+	 
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(define (make-exponentiation base exponent)
+  (list '** base exponent))
+
+(define (base exp)
+  (cadr exp))
+(define (exponent exp)
+  (caddr exp))
+(define (exponentiation? exp)
+  (and (pair? exp) (eq? (car exp) '**)))
+  
+
+
+
+
        
