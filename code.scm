946bb6a3b6744c51507a32957edbedcf5d8e6e22
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2023-09-21 10:00:05 +0530

    ex2.59 ex2.60

--- code.scm
+++ code.scm
@@ -1627,7 +1627,54 @@
         (else (list m1 '* m2))))
 
 
+;; When we turn to the representation of sets, the choice of a representation
+;; is not so obvious. Indeed, there are a number of possible representations,
+;; and they differ significantly from one another in several ways.
 
+
+;; sets as unordered lists
+;; ex 2.59
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)        
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+
+
+;; union-set
+(define (union-set set1 set2)
+  (cond
+   ((null? set1) set2)
+   ((element-of set? (car set1) set2)
+    (union-set (cdr set1) set2))
+   (else (cons (car set1)
+	       (union-set (cdr set1) set2)))))
+
+
+;; ex 2.60
+;; element-of-set? would still be the same
+(define (adjoin-set-duplicate x set)
+  (cons x set))
+;; intersection-set would still be the same
+(define (union-set-duplicate set1 set2)
+  (cond
+   ((null? set1) set2)
+   (else (cons (car set1)
+	       (union-set (cdr set1) set2)))))
 
 
 
