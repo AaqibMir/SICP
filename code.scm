6b6a2510c71551fb5f158fa1415ccd784ea93684
Aaqib Mir <25548689+AaqibMir@users.noreply.github.com>
2024-06-02 09:53:18 +0530

    ex 2.75

--- code.scm
+++ code.scm
@@ -1997,3 +1997,18 @@
     sha boom))
 
 (define encode-song (encode rock-song symbols-huffman-tree))
+
+
+;; ex 2.75
+
+(define (make-from-mag-ang r a)
+  (lambda (op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else (error "Unknown op --make-from-mag-ang" op)))))
+
+
+
+
